30a92140a035bea333e0376e8760cbeb4cad5111
Artur Henrique <artureio2@hotmail.com>
2021-03-05 14:59:26 -0300

    AWS Settings

--- DjangoAwarded/settings.py
+++ DjangoAwarded/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # configurações de email em modo desenvolvimento:
-MAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#MAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # configurações de email para deploy:
 """
@@ -149,3 +149,11 @@ EMAIL_PORT= 587
 EMAIL_USER_TLS =True
 EMAIL_HOST_PASSWORD = 'SUA SENHA'
 """
+
+AWS_ACCESS_KEY_ID = 'AKIAXEGSI6W5577HCFFZ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'awarded-artureio'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
