8aaa68d0b534ecb3ef46c253c64fc430df57e7fd
Artur Henrique <artureio2@hotmail.com>
2021-03-17 20:36:22 -0300

    recuperação de password e-mail

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     imagem = models.ImageField(default='default.jpg', upload_to='perfil')
@@ -9,12 +10,13 @@ class Perfil(models.Model):
     def __str__(self):
         return f'{self.usuario.username} Perfil'
 
-    def save(self, *args, **kwargs):
-        super(Perfil, self).save(*args, **kwargs)
 
-        img = Image.open(self.imagem.path)
-
-        if img.height > 300 or img.width >300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.imagem.path)
+#def save(self, *args, **kwargs):
+#    super(Perfil, self).save(*args, **kwargs)
+#
+#    img = Image.open(self.imagem.path)
+#
+#    if img.height > 300 or img.width > 300:
+#        output_size = (300, 300)
+#        img.thumbnail(output_size)
+#        img.save(self.imagem.path)
