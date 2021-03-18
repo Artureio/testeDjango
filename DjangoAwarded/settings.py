62dad223104be88da412f95cda63ee0fce369082
Artur Henrique <artureio2@hotmail.com>
2021-03-18 16:40:53 -0300

    'teste'

--- DjangoAwarded/settings.py
+++ DjangoAwarded/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('AWARDED_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['awarded-artur.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
